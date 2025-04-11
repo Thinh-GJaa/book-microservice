016d8120208ef1ff68437e76de1cfa2bd2ba4d8e
Thinh_GJaa <xthinh4052002@gmail.com>
2025-04-11 09:46:38 +0700

    setup

---
+++ user-service/src/main/java/com/book/userservice/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.book.userservice.controller;
+
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "welcome to user service";
+    }
+
+}
