52f08f2d5e0d0b59adda6339e123df085785bb34
Thinh_GJaa <xthinh4052002@gmail.com>
2025-05-13 10:08:08 +0700

    add notification service

---
+++ notification-service/src/main/java/com/book/notificationservice/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package com.book.notificationservice.config;
+
+import com.book.notificationservice.dto.ApiResponse;
+import com.book.notificationservice.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException {
+        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+
+        response.setStatus(errorCode.getCode());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .message(errorCode.getMessageTemplate())
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
+    }
+}
