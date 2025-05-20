ac81b250e5dc6c18065d86f7547c91ee4ebc605b
Thinh_GJaa <xthinh4052002@gmail.com>
2025-05-20 10:04:05 +0700

    add config for profile service

---
+++ profile-service/src/main/java/com/book/profileservice/config/RedisConfig.java
@@ -0,0 +1,46 @@
+package com.book.profileservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30)) // TTL = 30 phút
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+
+
+    //Cấu hình riêng cho từng cache
+//    @Bean
+//    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+//        Map<String, RedisCacheConfiguration> cacheConfigs = new HashMap<>();
+//
+//        cacheConfigs.put("products", RedisCacheConfiguration.defaultCacheConfig()
+//                .entryTtl(Duration.ofMinutes(30)));
+//
+//        cacheConfigs.put("users", RedisCacheConfiguration.defaultCacheConfig()
+//                .entryTtl(Duration.ofHours(1)));
+//
+//        return RedisCacheManager.builder(connectionFactory)
+//                .withInitialCacheConfigurations(cacheConfigs)
+//                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig())
+//                .build();
+//    }
+
+}
