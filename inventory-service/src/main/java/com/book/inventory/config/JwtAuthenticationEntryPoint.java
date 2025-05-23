dc6a4f0baea2a5d2ba5368b308ec3106477663a5
Thinh_GJaa <xthinh4052002@gmail.com>
2025-05-23 15:13:32 +0700

    setup for inventory-service

---
+++ inventory-service/src/main/java/com/book/inventory/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,45 @@
+package com.book.inventory.config;
+
+import com.book.inventory.dto.ErrorResponse;
+import com.book.inventory.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // Xử lí các yêu cầu không có token hoặc không hợp lệ
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException {
+        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+
+        log.error("Lỗi JwtAuthenticationEntryPoint");
+        response.setStatus(errorCode.getStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ErrorResponse errorResponse = ErrorResponse.of(
+                String.valueOf(errorCode.getCode()),
+                errorCode.getStatus(),
+                errorCode.getMessageTemplate(),
+                null);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule()); // Đăng ký module JavaTimeModule
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // Tùy chọn không dùng timestamps
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        response.flushBuffer();
+    }
+}
---
+++ inventory-service/src/main/java/com/book/inventory/config/RedisConfig.java
@@ -0,0 +1,50 @@
+package com.book.inventory.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+        // Cấu hình riêng cho từng cache
+        @Bean
+        public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+                Map<String, RedisCacheConfiguration> cacheConfigs = new HashMap<>();
+
+                cacheConfigs.put("categories", RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofMinutes(10)));
+
+                cacheConfigs.put("author", RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofMinutes(10)));
+
+                cacheConfigs.put("authors", RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofMinutes(30)));
+
+                cacheConfigs.put("product", RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofMinutes(30)));
+
+                cacheConfigs.put("products", RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofMinutes(30)));
+
+                cacheConfigs.put("products_by_category", RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofMinutes(30)));
+
+                cacheConfigs.put("products_by_author", RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofMinutes(30)));
+
+                return RedisCacheManager.builder(connectionFactory)
+                                .withInitialCacheConfigurations(cacheConfigs)
+                                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig())
+                                .build();
+        }
+
+}
